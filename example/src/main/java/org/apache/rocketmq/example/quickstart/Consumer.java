71b6665896fcd7e442807f436f91b093c799564a
xu zheng yang <zhengyang.xu@amh-group.com>
2023-05-05 11:22:19 +0800

    修改example

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -21,6 +21,10 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This example shows how to subscribe and consume messages using providing {@link DefaultMQPushConsumer}.
@@ -52,6 +56,7 @@ public class Consumer {
         // Uncomment the following line while debugging, namesrvAddr should be set to your local address
 //        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
 
+        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
         /*
          * Specify where to start in case the specific consumer group is a brand-new one.
          */
@@ -67,6 +72,10 @@ public class Consumer {
          */
         consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
             System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
+            for (MessageExt messageExt : msg) {
+                String body = new String(messageExt.getBody(), StandardCharsets.UTF_8);
+                System.out.printf("Body: %s", body);
+            }
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
---
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/ProducerUseSelectQueue.java
@@ -0,0 +1,63 @@
+package org.apache.rocketmq.example.quickstart;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
+
+/**
+ * @author : XuZhengYang
+ * @date : 2023/5/5 9:33
+ * @description
+ */
+public class ProducerUseSelectQueue {
+
+
+    /**
+     * The number of produced messages.
+     */
+    public static final int MESSAGE_COUNT = 8;
+    public static final String PRODUCER_GROUP = "please_rename_unique_group_name";
+    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String TOPIC = "TopicTest";
+    public static final String TAG = "TagA";
+
+    public static void main(String[] args) throws MQClientException, InterruptedException {
+
+        DefaultMQProducer producer = new DefaultMQProducer(PRODUCER_GROUP);
+        producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
+        producer.start();
+
+        for (int i = 0; i < MESSAGE_COUNT; i++) {
+            try {
+                Message msg = new Message(TOPIC /* Topic */,
+                        TAG /* Tag */,
+                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+                );
+                SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                    @Override
+                    public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                        Integer id = (Integer) arg;
+                        int index = id % mqs.size();
+                        return mqs.get(index);
+                    }
+                }, i % 4);
+
+                System.out.printf("%s%n", sendResult);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
+
+        /*
+         * Shut down once the producer instance is no longer in use.
+         */
+        producer.shutdown();
+    }
+}
